ec2f3010c4f1408334790be826ed90dff77d98f8
kaypee <kaypee.casensa@gmail.com>
2019-06-13 11:33:12 +0800

    Display society list as json in society_list.html

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# SOCIETY
 urlpatterns = [
     path('society/', views.society_list, name='society_list'),
+    path('society-list-json/', views.SocietyListJson.as_view(), name='society_list_json'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,45 @@
 from django.shortcuts import render
+from django.utils.timezone import localtime
+from django_datatables_view.base_datatable_view import BaseDatatableView
 from .models import Participant, Convention, Society, Membership
 
 def home(request):
     return render(request, 'index.html', {})
+
+class SocietyListJson(BaseDatatableView):
+    model = Society
+    columns = ['name', 'date_created', 'date_updated', 'action']
+    order_columns = ['name', 'date_created', 'date_updated', '']
+
+    # exclude is_archived = True in datatables
+    def get_initial_queryset(self):
+        return Society.objects.all()
+    
+    def get_filter_method(self):
+        """ Returns preferred filter method """
+        return self.FILTER_ICONTAINS
+
+    def filter_queryset(self, qs):
+        search = self.request.GET.get('search[value]', None)
+
+        if search:
+            qs = qs.filter(name__icontains=search)
+        return qs
+
+    def render_column(self, row, column):
+        if column == 'action':
+            return """
+                <button class='btn btn-default m-0 p-0' data-toggle='tooltip' title='Update'>
+                    <i class='far fa-edit text-primary'></i>
+                </button>
+            """ # % (row.society_id) # create action buttons
+        elif column == 'date_created':
+            return "%s" % localtime(row.date_created).strftime("%Y-%m-%d %H:%M") # format date_created to "YYYY-MM-DD HH:mm"
+        elif column == 'date_updated':
+            # return "%s" % row.date_updated.strftime("%Y-%m-%d %H:%M") # format date_updated to "YYYY-MM-DD HH:mm"
+            return "%s" % localtime(row.date_updated).strftime("%Y-%m-%d %H:%M") # format date_updated to "YYYY-MM-DD HH:mm"
+        else:
+            return super(SocietyListJson, self).render_column(row, column)
 
 def society_list(request):
 
