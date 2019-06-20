a663bd38a1ad8ab2bdc1979dfe9a4f791e8edad6
kaypee <kaypee.casensa@gmail.com>
2019-06-20 14:46:50 +0800

    Set real-time date and time to attendance.html

--- assets/base/js/attendance/attendance.js
+++ assets/base/js/attendance/attendance.js
@@ -1,6 +1,15 @@
 $(function (){
     // focus cursor on search textbox
     $('.search-textbox', this).focus()
+    
+    // real-time date and time
+    setInterval(function() {
+        var momentNow = moment();
+        $('#convention_current_date').html(momentNow.format('YYYY MMMM DD') + ' '
+                            + momentNow.format('dddd')
+                             .substring(0,3));
+        $('#convention_current_time').html(momentNow.format('hh:mm:ss A'));
+    }, 100);
 
     var checkAttendance = function(e){
         e.preventDefault();
