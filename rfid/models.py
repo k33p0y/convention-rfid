a984460e3b0cbe7cbe38c7d4c096456f136ef0ff
kaypee <kaypee.casensa@gmail.com>
2019-07-11 14:50:17 +0800

    Create Rfid, Convention, Society and Participant models

--- rfid/models.py
+++ rfid/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+class Society(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Society'
+        verbose_name_plural = 'Societies'
+
+    def __str__(self):
+        return self.name
+
+class Participant(models.Model):
+    fname = models.CharField('First name', max_length=100)
+    mname = models.CharField('Middle name', max_length=100, blank=True)
+    lname = models.CharField('Last name', max_length=100)
+    prc_num = models.CharField('PRC#', max_length=15, unique=True)
+    birthdate = models.DateField(blank=True, null=True)
+    address = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '%s %s' % (self.fname, self.lname)
+
+class Rfid(models.Model):
+    rfid_num = models.CharField('RFID', max_length=15)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    society = models.ForeignKey(Society, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        unique_together = ('rfid_num', 'society',)
+    
+    def __str__(self):
+        return self.rfid_num
+
+class Convention(models.Model):
+    name = models.CharField(max_length=250)
+    date_start = models.DateField()
+    date_end = models.DateField(blank=True, null=True)
+    venue = models.TextField(max_length=250)
+    is_open = models.BooleanField(default=True)
+    rfids = models.ManyToManyField(Rfid)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+
+# class Attendance(models.Model):
+#     attendance_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     rfid = models.ForeignKey(Rfid, on_delete=models.CASCADE)
+#     convention = models.ForeignKey(Convention, on_delete=models.CASCADE)
+#     date_created = models.DateField(auto_now_add=True)
+#     check_in = models.TimeField(auto_now_add=True)
+#     check_out = models.TimeField(blank=True, null=True)
+
+#     def __str__(self):
+#         return '%s-%s' % (self.rfid, self.convention)
