5752e549e512e5f08ede7ccbf0f77061116ed789
kaypee <kaypee.casensa@gmail.com>
2019-07-12 17:04:41 +0800

    Detect keyboard number input in participant_check_in.html

--- assets/base/js/rfid/check-in.js
+++ assets/base/js/rfid/check-in.js
@@ -7,4 +7,22 @@ $(function (){
                             .substring(0,3));
         $('#convention_current_time').html(momentNow.format('hh:mm:ss A'));
     }, 100);
-})
+
+    var rfid_num = '';
+    $(document).keypress(function(event){
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (event.keyCode >= 48 && event.keyCode <= 57){
+            // console.log(event.which);
+            // console.log(String.fromCharCode(event.keyCode))
+            rfid_num += String.fromCharCode(event.keyCode)
+        }
+        // if (event.keyCode >= 65 && event.keyCode <= 90)
+
+        if (rfid_num){
+            if(keycode == '13'){
+                alert(rfid_num);
+                rfid_num = '';
+            }
+        }
+    });
+})  
