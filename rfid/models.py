0d24f20c4bbdc0c33d64b0400a3b386d676dde66
kaypee <kaypee.casensa@gmail.com>
2019-07-22 11:44:26 +0800

    Create model Occupation

--- rfid/models.py
+++ rfid/models.py
@@ -66,3 +66,11 @@ class Attendance(models.Model):
 
     def __str__(self):
         return '%s-%s' % (self.rfid, self.convention)
+
+class Occupation(models.Model):
+    name = models.CharField(max_length=250)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
